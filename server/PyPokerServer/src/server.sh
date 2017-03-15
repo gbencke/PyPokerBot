26f5862f91df3d81f8780ae8191fd7244422aa5c
Guilherme Bencke <gbencke@gmail.com>
2017-03-14 21:33:16 -0300

    Ajuste no path para startar o server

--- server/PyPokerServer/src/server.sh
+++ server/PyPokerServer/src/server.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd /home/gbencke/git/2017.03.14.PyPokerBot/server/PyPokerServer/src 
 export LD_LIBRARY_PATH=/home/gbencke/git/pbots_calc/export/linux2/lib:$LD_LIBRARY_PATH
 export FLASK_APP=server.py
 export FLASK_DEBUG=1
