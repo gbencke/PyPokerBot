aed9aed873c7ef172e9c1e0e8386621e3d00bef5
Guilherme Bencke <gbencke@gmail.com>
2017-03-15 16:45:40 -0300

    Capturing multiple tables

--- client/PyPokerBotClient/src/helpers/win32/capture_screenshot.py
+++ client/PyPokerBotClient/src/helpers/win32/capture_screenshot.py
@@ -1,8 +1,12 @@
 import logging
 import pyscreenshot as ImageGrab
+import win32gui
 
 
 def capture_screenshot(hwnd, file_to_save):
     logging.debug("Capturing {} to {}".format(hwnd, file_to_save))
+    win32gui.SetForegroundWindow(hwnd)
+    rect = win32gui.GetWindowRect(hwnd)
     im = ImageGrab.grab()
-    im.show()
+    im = im.crop((rect[0] * 1.25, rect[1] *1.25, rect[2] * 1.25, rect[3] * 1.25 ))
+    im.save(file_to_save)
---
+++ client/PyPokerBotClient/src/platforms/pokerstarts/PokerStarsInterface.py
@@ -0,0 +1,3 @@
+
+class PokerStarsInterface:
+    pass
