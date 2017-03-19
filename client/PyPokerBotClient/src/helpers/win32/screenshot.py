27dcade329a3b9099ef964c52b1cdb31fe194620
Guilherme Bencke <gbencke@gmail.com>
2017-03-19 19:57:45 -0300

    New resolutions and fixes
    Former-commit-id: b2f0a152c3ad2b607a2da2c7b475f795c49a3c1c

--- client/PyPokerBotClient/src/helpers/win32/screenshot.py
+++ client/PyPokerBotClient/src/helpers/win32/screenshot.py
@@ -8,7 +8,10 @@ def capture_screenshot(hwnd, file_to_save):
     logging.debug("Capturing {} to {}".format(hwnd, file_to_save))
     try:
         win32gui.SetForegroundWindow(hwnd)
+        win32gui.MoveWindow(hwnd, 0, 0, 1042, 745, True)
         rect = win32gui.GetWindowRect(hwnd)
+        #print rect
+        #exit(0)
         im = ImageGrab.grab()
         im = im.crop((rect[0] * 1.25, rect[1] * 1.25, rect[2] * 1.25, rect[3] * 1.25))
         im.save(file_to_save)
