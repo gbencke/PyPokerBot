396768889ce48d642b9e84d14b54c4e95f049bbf
Guilherme Bencke <gbencke@gmail.com>
2017-03-21 17:18:42 -0300

    Added T6 and T7 to the card ranges

--- server/PyPokerServer/src/helpers/RangeHelper.py
+++ server/PyPokerServer/src/helpers/RangeHelper.py
@@ -7,7 +7,10 @@ class RangeHelper:
                  ['AKo','AQs','AJs','KQs','TT'],
                  ['AQo','ATs','KJs','JTs','99'],
                  ['AJo','KQo','KTs','QTs','J9s','T9s','98s','88'],
-                 ['A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','KJo','QJo','JTo','97s','87s','77','76s','66']]
+                 ['A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','KJo','QJo','JTo','97s','87s','77','76s','66'],
+                 ['ATo','KTo','QTo','J8s','86s','75s','65s','55','45s'],
+                 ['K9s','K8s','K7s','K6s','K5s','K4s','K3s','K2s','J9o','T9o','98o','64s','53s','44','43s','33','22']
+               ]
 
 
     def __init__(self):
@@ -30,6 +33,10 @@ class RangeHelper:
             return self.get_group(4)
         if str == 'T5':
             return self.get_group(5)
+        if str == 'T6':
+            return self.get_group(6)
+        if str == 'T7':
+            return self.get_group(7)
         return [str]
 
     def parse(self, str):
