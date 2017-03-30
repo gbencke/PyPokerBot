7788b260e8eda5a408c50152877b52c1e8e83dfe
Guilherme Bencke <gbencke@gmail.com>
2017-03-30 18:02:53 -0300

    now working with major refactoring

--- client/PyPokerBotClient/src/model/PokerStrategySimple.py
+++ client/PyPokerBotClient/src/model/PokerStrategySimple.py
@@ -13,13 +13,12 @@ class PokerStrategySimple(PokerStrategy):
 
         total_villains = 0
         for x in range(analisys['seats']):
-            current_seat = x + 1
-            if analisys['cards']['PLAYER{}_HASCARD'.format(current_seat)] == 'CARD':
+            if analisys['cards'][x] == 'CARD':
                 total_villains += 1
         if total_villains == 1:
             return 'CONFIDENCE_LEVEL_HEADS_UP'
         else:
-            if analisys['hero']['POSITION'] == 'BUTTON':
+            if analisys['hero']['position'] == 'BUTTON':
                 return 'CONFIDENCE_LEVEL_POSITION'
             else:
                 return 'CONFIDENCE_LEVEL'
@@ -30,16 +29,16 @@ class PokerStrategySimple(PokerStrategy):
 
         confidence_level = settings['STRATEGIES']['SIMPLE'][phase][self.get_confidence_level(analisys, phase)]
         confidence_level_raise = settings['STRATEGIES']['SIMPLE'][phase]['CONFIDENCE_DIFFERENCE_RAISE']
-        hand_equity = analisys['hand_analisys']['RESULT'][0][1]
-        ret['RAISE_STRATEGY'] = settings['STRATEGY']['FLOP']['RAISE_STRATEGY']
+        hand_equity = analisys['hand_analisys']['result'][0][1]
+        ret['raise_strategy'] = settings['STRATEGY']['FLOP']['RAISE_STRATEGY']
         if hand_equity >= (confidence_level + confidence_level_raise):
-            ret['DECISION'] = 'RAISE'
+            ret['decision'] = 'RAISE'
             return ret
         if hand_equity >= confidence_level:
-            ret['DECISION'] = 'CALL'
+            ret['decision'] = 'CALL'
             return ret
         else:
-            ret['DECISION'] = 'FOLD OR CHECK'
+            ret['decision'] = 'FOLD OR CHECK'
         return ret
 
     def generate_decision_flop(self, analisys):
@@ -48,78 +47,78 @@ class PokerStrategySimple(PokerStrategy):
 
         confidence_level =  settings['STRATEGIES']['SIMPLE'][phase][self.get_confidence_level(analisys, phase)]
         confidence_level_raise =  settings['STRATEGIES']['SIMPLE'][phase]['CONFIDENCE_DIFFERENCE_RAISE']
-        hand_equity = analisys['hand_analisys']['RESULT'][0][1]
-        is_hero_in_button = (analisys['hero']['POSITION'] == 'BUTTON')
+        hand_equity = analisys['hand_analisys']['result'][0][1]
+        is_hero_in_button = (analisys['hero']['position'] == 'BUTTON')
         check_button_available = self.verify_check_command(analisys)
 
         ret['RAISE_STRATEGY'] = settings['STRATEGIES']['SIMPLE'][phase]['RAISE_STRATEGY']
         if hand_equity >= (confidence_level + confidence_level_raise):
-            ret['DECISION'] = 'RAISE'
+            ret['decision'] = 'RAISE'
             return ret
         if hand_equity >= confidence_level:
-            ret['DECISION'] = 'CALL'
+            ret['decision'] = 'CALL'
             return ret
         else:
             if is_hero_in_button and check_button_available and hand_equity > 0.5:
                 logging.debug('BLUFFING!!! BLUFFING!!! BLUFFING!!! BLUFFING!!! BLUFFING!!!')
-                ret['DECISION'] = 'RAISE'
+                ret['decision'] = 'RAISE'
             else:
-                ret['DECISION'] = 'FOLD OR CHECK'
+                ret['decision'] = 'FOLD OR CHECK'
 
         return ret
 
     def generate_decision(self, analisys):
         ret = {}
         try:
-            phase = analisys['hand_analisys']['HAND_PHASE']
+            phase = analisys['hand_analisys']['hand_phase']
             if phase == 'PREFLOP':
                 return self.generate_decision_preflop(analisys)
             else:
                 return self.generate_decision_flop(analisys)
         except:
-            ret['DECISION'] = 'FOLD OR CHECK'
+            ret['decision'] = 'FOLD OR CHECK'
 
         return ret
 
     def generate_command(self, analisys):
-        ret = {'TO_EXECUTE': 0}
+        ret = {'to_execute': 0}
 
         if not self.has_command_to_execute(analisys):
             return ret
 
-        if analisys['decision']['DECISION'] == 'FOLD OR CHECK':
+        if analisys['decision']['decision'] == 'FOLD OR CHECK':
             for x in range(3):
-                if 'CHECK' in analisys['commands']['COMMAND{}'.format(x + 1)].upper():
-                    ret['TO_EXECUTE'] = x + 1
+                if 'CHECK' in analisys['commands'][x].upper():
+                    ret['to_execute'] = x + 1
                     return ret
             for x in range(3):
-                if 'FOLD' in analisys['commands']['COMMAND{}'.format(x + 1)].upper():
-                    ret['TO_EXECUTE'] = x + 1
-                    return ret
-
-        if analisys['decision']['DECISION'] == 'RAISE':
-            for x in range(3):
-                if ('RAISE' in analisys['commands']['COMMAND{}'.format(x + 1)].upper() or
-                            'BET' in analisys['commands']['COMMAND{}'.format(x + 1)].upper()):
-                    ret['TO_EXECUTE'] = x + 1
-                    return ret
-            for x in range(3):
-                if 'CALL' in analisys['commands']['COMMAND{}'.format(x + 1)].upper():
-                    ret['TO_EXECUTE'] = x + 1
-                    return ret
-            for x in range(3):
-                if 'CHECK' in analisys['commands']['COMMAND{}'.format(x + 1)].upper():
-                    ret['TO_EXECUTE'] = x + 1
+                if 'FOLD' in analisys['commands'][x].upper():
+                    ret['to_execute'] = x + 1
                     return ret
 
-        if analisys['decision']['DECISION'] == 'CALL':
+        if analisys['decision']['decision'] == 'RAISE':
             for x in range(3):
-                if 'CALL' in analisys['commands']['COMMAND{}'.format(x + 1)].upper():
-                    ret['TO_EXECUTE'] = x + 1
+                if ('RAISE' in analisys['commands'][x].upper() or
+                            'BET' in analisys['commands'][x].upper()):
+                    ret['to_execute'] = x + 1
                     return ret
             for x in range(3):
-                if 'CHECK' in analisys['commands']['COMMAND{}'.format(x + 1)].upper():
-                    ret['TO_EXECUTE'] = x + 1
+                if 'CALL' in analisys['commands'][x].upper():
+                    ret['to_execute'] = x + 1
+                    return ret
+            for x in range(3):
+                if 'CHECK' in analisys['commands'][x].upper():
+                    ret['to_execute'] = x + 1
+                    return ret
+
+        if analisys['decision']['decision'] == 'CALL':
+            for x in range(3):
+                if 'CALL' in analisys['commands'][x].upper():
+                    ret['to_execute'] = x + 1
+                    return ret
+            for x in range(3):
+                if 'CHECK' in analisys['commands'][x].upper():
+                    ret['to_execute'] = x + 1
                     return ret
 
         return ret
