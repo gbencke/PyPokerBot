f767fa03a29178a3b41851f333660539e6d9b8c3
Guilherme Bencke <gbencke@gmail.com>
2017-03-31 12:14:27 -0300

    more fixes on bet scanning
    Former-commit-id: c32a311bb5403d6ac9347336e290fb5bd3db3714

--- client/PyPokerBotClient/src/model/PokerTableScanner.py
+++ client/PyPokerBotClient/src/model/PokerTableScanner.py
@@ -1,13 +1,40 @@
-
-
 class PokerTableScanner:
+    suits = ['h', 's', 'c', 'd']
+    cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
     def __init__(self, TableType, NumberOfSeats, BB, SB):
         self.TableType = TableType
         self.NumberOfSeats = NumberOfSeats
         self.BB = BB
         self.SB = SB
 
-
-    suits = ['h', 's', 'c', 'd']
-    cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-
+    def get_suite_from_image(self, selected_image):
+        red = 0
+        blue = 0
+        green = 0
+        black = 0
+        for w in range(selected_image.width):
+            for h in range(selected_image.height):
+                pixel = selected_image.getpixel((w, h))
+                if pixel[0] >= 230 and pixel[1] >= 230 and pixel[2] >= 230:
+                    continue
+                if pixel[0] < 20 and pixel[1] < 20 and pixel[2] < 20:
+                    black += 1
+                    continue
+                if pixel[0] >= pixel[1] and pixel[0] >= pixel[2]:
+                    red += 1
+                    continue
+                if pixel[1] >= pixel[0] and pixel[1] >= pixel[2]:
+                    green += 1
+                    continue
+                if pixel[2] >= pixel[0] and pixel[2] >= pixel[1]:
+                    blue += 1
+                    continue
+        if black > 200:
+            return 's'
+        if red > green and red > blue and red > black:
+            return 'h'
+        if green > red and green > blue and green > black:
+            return 'c'
+        if blue > red and blue > green and blue > black:
+            return 'd'
--- client/PyPokerBotClient/src/platforms/pokerstars/PokerTableScannerPokerStars.py
+++ client/PyPokerBotClient/src/platforms/pokerstars/PokerTableScannerPokerStars.py
@@ -164,36 +164,7 @@ class PokerTableScannerPokerStars(PokerTableScanner):
             ret[index] = selected_card
         return ret
 
-    def get_suite_from_image(self, selected_image):
-        red = 0
-        blue = 0
-        green = 0
-        black = 0
-        for w in range(selected_image.width):
-            for h in range(selected_image.height):
-                pixel = selected_image.getpixel((w, h))
-                if pixel[0] >= 230 and pixel[1] >= 230 and pixel[2] >= 230:
-                    continue
-                if pixel[0] < 20 and pixel[1] < 20 and pixel[2] < 20:
-                    black += 1
-                    continue
-                if pixel[0] >= pixel[1] and pixel[0] >= pixel[2]:
-                    red += 1
-                    continue
-                if pixel[1] >= pixel[0] and pixel[1] >= pixel[2]:
-                    green += 1
-                    continue
-                if pixel[2] >= pixel[0] and pixel[2] >= pixel[1]:
-                    blue += 1
-                    continue
-        if black > 200:
-            return 's'
-        if red > green and red > blue and red > black:
-            return 'h'
-        if green > red and green > blue and green > black:
-            return 'c'
-        if blue > red and blue > green and blue > black:
-            return 'd'
+
 
     def get_hero_position(self, hero_pos, cards, button):
         if button[hero_pos]:
@@ -388,7 +359,7 @@ class PokerTableScannerPokerStars(PokerTableScanner):
                 ret = self.non_decimal.sub('', ret)
                 returned_string = ret
                 ret = float(ret) / self.BB
-                if ((float(int(ret)) - float(ret)) != 0) and (not (ret == 0.5)) or ret > 200:
+                if ((float(int(ret)) - float(ret)) != 0) and (not (ret == 0.5)) or ret > 200 or ret == 0:
                     continue
                 returned_list[x] = (ret, returned_string, return_from_tesseract)
                 break
