ecd4b141f1d3a649d2e28a374a0db6d28406b5a6
Guilherme Bencke <gbencke@gmail.com>
2017-04-01 21:35:52 -0300

    Creating the final string and logging it
    Former-commit-id: 92b9c1c02df34ce82eb50f2062fb87982780490d

--- client/PyPokerBotClient/src/tasks/analyse_table.py
+++ client/PyPokerBotClient/src/tasks/analyse_table.py
@@ -1,8 +1,7 @@
 import importlib
-import pprint
-import logging
 from settings import settings
 from osinterface.win32.screenshot import grab_image_from_file
+from model.PokerTableScanner import PokerTableScanner
 
 
 def get_instance(classname):
@@ -25,4 +24,17 @@ def execute(args):
     im = grab_image_from_file(image_name)
     result = table_scanner.analyze_from_image(im)
     result = table_strategy.run_strategy(result)
-    logging.debug(pprint.pformat(result))
+    final_analisys = ''
+    final_analisys += '====================================\n'
+    final_analisys += 'Command           :{}\n'.format(result['commands'][result['command']['to_execute'] - 1])
+    final_analisys += 'Decision          :{}\n'.format(result['decision']['decision'])
+    final_analisys += '------------------------------------\n'
+    final_analisys += 'Number of Villains:{}\n'.format(len([x for x in result['cards'] if x]))
+    final_analisys += 'Flop              :{}\n'.format("".join(result['flop']))
+    final_analisys += 'Pocket Cards      :{}\n'.format(result['hero']['hero_cards'])
+    final_analisys += 'Position          :{}\n'.format(result['hero']['position'])
+    final_analisys += 'Equity            :{}'.format(result['hand_analisys']['result'])
+    PokerTableScanner.generate_analisys_summary(final_analisys)
+
+
+
