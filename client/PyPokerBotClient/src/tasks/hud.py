bf0f793523e7a90e9d0dbaaba2a81c78db7c41f0
Guilherme Bencke <gbencke@gmail.com>
2017-04-02 11:00:47 -0300

    HUD seems to be working...
    Former-commit-id: 7963aea6590a361624a424eebb0bf014635eccad

--- client/PyPokerBotClient/src/tasks/hud.py
+++ client/PyPokerBotClient/src/tasks/hud.py
@@ -1,8 +1,9 @@
 import os
+import logging
 from time import sleep
 from settings import settings
 from osinterface.win32.screenshot import capture_screenshot
-from osinterface.win32.hwnd_check import is_minimized,is_window_with_focus
+from osinterface.win32.hwnd_check import is_minimized, is_window_with_focus
 from model.PokerBot import PokerBot
 from model.PokerTableScanner import PokerTableScanner
 
@@ -14,36 +15,50 @@ def get_time_to_sleep():
 def execute(args):
     analisys = ''
     while True:
-        lobbies = PokerBot.scan_for_lobbies()
-        for current_lobby in lobbies:
-            for current_table in current_lobby.get_tables():
-                sleep(get_time_to_sleep())
-                if is_minimized(current_table.hwnd):
-                    continue
-                if not is_window_with_focus(current_table.hwnd):
-                    continue
-                im = capture_screenshot(current_table.hwnd,
-                                        os.path.join(settings['SAMPLES_FOLDER'],
-                                                     current_table.get_screenshot_name()))
-                result = current_table.refresh_from_image(im)
-                result = current_table.generate_decision(result)
-                if True or current_table.has_command_to_execute(result):
-                    final_analisys = ''
-                    final_analisys += '====================================\n'
-                    if current_table.has_command_to_execute(result):
-                        final_analisys += 'Command           :{}\n'.format(result['commands'][result['command']['to_execute'] - 1])
-                        final_analisys += 'Decision          :{}\n'.format(result['decision']['decision'])
-                    final_analisys += '------------------------------------\n'
-                    final_analisys += 'Number of Villains:{}\n'.format(len([x for x in result['cards'] if x]))
-                    final_analisys += 'Flop              :{}\n'.format("".join(result['flop']))
-                    final_analisys += 'Pocket Cards      :{}\n'.format(result['hero']['hero_cards'])
-                    final_analisys += 'Position          :{}\n'.format(result['hero']['position'])
-                    if current_table.has_command_to_execute(result):
-                        final_analisys += 'Equity            :{}'.format(result['hand_analisys']['result'])
-                    if final_analisys == analisys:
+        try:
+            lobbies = PokerBot.scan_for_lobbies()
+            for current_lobby in lobbies:
+                for current_table in current_lobby.get_tables():
+                    sleep(get_time_to_sleep())
+                    if is_minimized(current_table.hwnd):
                         continue
-                    analisys = final_analisys
-                    PokerTableScanner.generate_analisys_summary(final_analisys.strip())
-                    im.save(os.path.join(settings['SAMPLES_FOLDER'], current_table.get_screenshot_name()))
-                    #run_command(current_table.hwnd, res['command']['TO_EXECUTE'], res)
+                    if not is_window_with_focus(current_table.hwnd):
+                        continue
+                    im = capture_screenshot(current_table.hwnd,
+                                            os.path.join(settings['SAMPLES_FOLDER'],
+                                                         current_table.get_screenshot_name()))
+                    result = current_table.refresh_from_image(im)
+                    result = current_table.generate_decision(result)
+                    if True or current_table.has_command_to_execute(result):
+                        final_analisys = ''
+                        final_analisys += '==========================================================\n'
+                        final_analisys += '==========================================================\n'
+                        if current_table.has_command_to_execute(result):
+                            final_analisys += 'Command           :{}\n'.format(
+                                result['commands'][result['command']['to_execute'] - 1])
+                            final_analisys += 'Decision          :{}\n'.format(result['decision']['decision'])
+                            final_analisys += 'Equity            :{}'.format(result['hand_analisys']['result'])
+                        final_analisys += '----------------------------------------------------------\n'
+                        final_analisys += 'Number of Villains:{}\n'.format(len([x for x in result['cards'] if x]))
+                        final_analisys += 'Flop              :{}\n'.format("".join(result['flop']))
+                        final_analisys += 'Pocket Cards      :{}\n'.format(result['hero']['hero_cards'])
+                        final_analisys += 'Position          :{}\n'.format(result['hero']['position'])
+                        if final_analisys == analisys:
+                            continue
+                        analisys = final_analisys
+                        PokerTableScanner.generate_analisys_summary(final_analisys.strip())
+                        im.save(os.path.join(settings['SAMPLES_FOLDER'], current_table.get_screenshot_name()))
+            if len(lobbies) == 0:
+                logging.debug('No Lobbies, sleeping 1 sec')
+                sleep(1)
+                continue
+            else:
+                if len(lobbies[0].get_tables()) == 0:
+                    logging.debug('No Tables, sleeping 1 sec')
+                    sleep(1)
+                    continue
+        except Exception as e:
+            logging.debug('error:' + str(e))
+
+
 
---
+++ client/test.bat
@@ -0,0 +1,7 @@
+
+echo "Starting virtualenv"
+call C:\git\2017.03.14.PyPokerBot\client\PyPokerBotClient\Scripts\activate.bat
+echo "going to Start"
+cd C:\git\2017.03.14.PyPokerBot\client\PyPokerBotClient\src
+echo "Starting HUD"
+start pythonw PyPokerBot.py hud
