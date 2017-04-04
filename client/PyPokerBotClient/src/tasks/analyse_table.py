025bf634ff317b9b8aac9ac0bf3c3468752b7745
Guilherme Bencke <gbencke@gmail.com>
2017-04-04 09:39:26 -0300

    Now, it only works if it has a command...
    Former-commit-id: 0e935d5bc4bbd3204902d6525d8b7b75f25a10bb

--- client/PyPokerBotClient/src/tasks/analyse_table.py
+++ client/PyPokerBotClient/src/tasks/analyse_table.py
@@ -1,8 +1,7 @@
 import importlib
 from settings import settings
 from osinterface.win32.screenshot import grab_image_from_file
-from model.PokerTableScanner import PokerTableScanner
-
+from model.PokerTableScanner import PokerTableScanner, has_command_to_execute
 
 def get_instance(classname):
     return getattr(importlib.import_module(classname), classname.split('.')[-1])
@@ -23,16 +22,17 @@ def execute(args):
 
     im = grab_image_from_file(image_name)
     result = table_scanner.analyze_from_image(im)
-    result = table_strategy.run_strategy(result)
-    final_analisys = ''
-    final_analisys += '====================================\n'
-    final_analisys += 'Command           :{}\n'.format(result['commands'][result['command']['to_execute'] - 1])
-    final_analisys += 'Decision          :{}({})\n'.format(result['decision']['decision'],
-                                                           result['decision']['raise_strategy'])
-    final_analisys += '------------------------------------\n'
-    final_analisys += 'Number of Villains:{}\n'.format(len([x for x in result['cards'] if x]))
-    final_analisys += 'Flop              :{}\n'.format("".join(result['flop']))
-    final_analisys += 'Pocket Cards      :{}\n'.format(result['hero']['hero_cards'])
-    final_analisys += 'Position          :{}\n'.format(result['hero']['position'])
-    final_analisys += 'Equity            :{}'.format(result['hand_analisys']['result'])
-    PokerTableScanner.generate_analisys_summary(final_analisys)
+    if has_command_to_execute(result):
+        result = table_strategy.run_strategy(result)
+        final_analisys = ''
+        final_analisys += '====================================\n'
+        final_analisys += 'Command           :{}\n'.format(result['commands'][result['command']['to_execute'] - 1])
+        final_analisys += 'Decision          :{}({})\n'.format(result['decision']['decision'],
+                                                               result['decision']['raise_strategy'])
+        final_analisys += '------------------------------------\n'
+        final_analisys += 'Number of Villains:{}\n'.format(len([x for x in result['cards'] if x]))
+        final_analisys += 'Flop              :{}\n'.format("".join(result['flop']))
+        final_analisys += 'Pocket Cards      :{}\n'.format(result['hero']['hero_cards'])
+        final_analisys += 'Position          :{}\n'.format(result['hero']['position'])
+        final_analisys += 'Equity            :{}'.format(result['hand_analisys']['result'])
+        PokerTableScanner.generate_analisys_summary_info(final_analisys)
