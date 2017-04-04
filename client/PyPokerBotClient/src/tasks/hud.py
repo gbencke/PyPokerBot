98d1d96b9d16ba1bcf02340498d6866bde095c01
Guilherme Bencke <gbencke@gmail.com>
2017-04-04 10:34:06 -0300

    Some improvements on the hud
    Former-commit-id: 1c9d85ebf124ec683582d267c627c5d65f62b98d

--- client/PyPokerBotClient/src/tasks/hud.py
+++ client/PyPokerBotClient/src/tasks/hud.py
@@ -5,7 +5,7 @@ from settings import settings
 from osinterface.win32.screenshot import capture_screenshot
 from osinterface.win32.hwnd_check import is_minimized, is_window_with_focus
 from model.PokerBot import PokerBot
-from model.PokerTableScanner import PokerTableScanner
+from model.PokerTableScanner import PokerTableScanner, has_command_to_execute
 
 
 def get_time_to_sleep():
@@ -15,31 +15,36 @@ def get_time_to_sleep():
 def execute(args):
     logging.info("Starting HUD....")
     if len(args) > 0:
-        sleep(int(args[0]))
+        total_secs = int(args[0])
+        logging.info("Will sleep for {}....".format(args[0]))
+        for x in range(total_secs):
+            logging.info("Sleeping({})".format(total_secs - x))
+            sleep(1)
     analisys = ''
     while True:
         try:
+            sleep(get_time_to_sleep())
             lobbies = PokerBot.scan_for_lobbies()
             for current_lobby in lobbies:
                 for current_table in current_lobby.get_tables():
-                    sleep(get_time_to_sleep())
                     if is_minimized(current_table.hwnd):
                         continue
                     if not is_window_with_focus(current_table.hwnd):
                         continue
                     im = capture_screenshot(current_table.hwnd,
                                             os.path.join(settings['SAMPLES_FOLDER'],
-                                                         current_table.get_screenshot_name()))
+                                                         current_table.get_screenshot_name()), should_save=False)
                     result = current_table.refresh_from_image(im)
                     result = current_table.generate_decision(result)
-                    if current_table.has_command_to_execute(result):
-                        final_analisys = ''
+                    if has_command_to_execute(result):
+                        final_analisys = '\n'
                         final_analisys += '==========================================================\n'
                         final_analisys += '==========================================================\n'
                         final_analisys += 'Decision          :{}({})\n'.format(
                             result['decision']['decision'],
                             result['decision']['raise_strategy'])
-                        final_analisys += 'Equity            :{}\n'.format(int(result['hand_analisys']['result'][0][1]*100))
+                        final_analisys += 'Equity            :{}%\n'.format(
+                            int(result['hand_analisys']['result'][0][1] * 100))
                         final_analisys += '----------------------------------------------------------\n'
                         final_analisys += 'Number of Villains:{}\n'.format(len([x for x in result['cards'] if x]))
                         final_analisys += 'Flop              :{}\n'.format("".join(result['flop']))
@@ -59,7 +64,5 @@ def execute(args):
                     sleep(1)
                     continue
         except Exception as e:
+            im.save(os.path.join(settings['SAMPLES_FOLDER'], 'Error.' + current_table.get_screenshot_name()))
             logging.error('error:' + str(e))
-
-
-
