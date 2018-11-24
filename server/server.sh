3ef185ea496fef9b0a6026bad5dd971cfdbdd2c5
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-11-24 18:49:03 -0200

    Forced to run the server as root
    Former-commit-id: 9c53e38a53f285b42df84eb9ea483962fb6b5430

--- server/server.sh
+++ server/server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo ldconfig
-source /home/gbencke/git/2017.03.14.PyPokerBot/server/PyPokerServer/bin/activate
-cd /home/gbencke/git/2017.03.14.PyPokerBot/server/PyPokerServer/src 
-export LD_LIBRARY_PATH=/home/gbencke/git/pbots_calc/export/linux2/lib:$LD_LIBRARY_PATH
+if [ "$EUID" -ne 0 ]
+then echo "Please run as root"
+        exit
+fi
+
+cd src
 export FLASK_APP=server.py
 export FLASK_DEBUG=1
 flask run --host=0.0.0.0
