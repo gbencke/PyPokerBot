a9f0b0e7c150f0544aea816d224b2c523c7ce4ed
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-11-24 18:44:59 -0200

    Added the calculator shell to allow debugging only the .so
    Former-commit-id: 9f535aff82e157821e7632f86324d94bcdde3efd

---
+++ server/src/calculator.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#export LD_LIBRARY_PATH=/mnt/git/098.PyPokerBot/server/pbots_calc/export/linux2/lib:$LD_LIBRARY_PATH
+python /mnt/git/098.PyPokerBot/server/pbots_calc/python/calculator.py $@
