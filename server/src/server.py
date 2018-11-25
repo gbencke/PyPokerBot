af636fe5bf6371b96eb9c0257b25cedaba3b37a7
Guilherme Bencke <gbencke@benckesoftware.com.br>
2018-11-25 18:59:20 -0200

    Lint fixing

--- server/src/server.py
+++ server/src/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
-import pbots_calc
+from .calculator.pbots_calc import pbots_calc
 from helpers.RangeHelper import RangeHelper
 from helpers.LookupTable import LookupTable
-import lookup_table
+from .lookup_table.lookup_table import lookup_table
 
 app = Flask(__name__)
 lookup_table_utils = LookupTable()
+
 
 def normalize_cards(tokens):
     start_cards = (RangeHelper()).parse(tokens[0])
@@ -15,11 +16,12 @@ def normalize_cards(tokens):
     start_cards = ':'.join(start_cards_list)
     return start_cards
 
+
 @app.route('/calculator', methods=['POST'])
 def calculator():
     command = request.get_json()['command']
     tokens = command.split(' ')
-    start_cards = tokens[0] 
+    start_cards = tokens[0]
     board = ""
     dead = ""
     if len(tokens) > 1:
@@ -34,13 +36,13 @@ def calculator():
         start_cards = normalize_cards(tokens)
         for x in lookup_table.lookup_table.keys():
             print(str(lookup_table.lookup_table[x])[:80])
-            card_found = filter(lambda card: card['cards'] == start_cards, lookup_table.lookup_table[x])
+            card_found = filter(lambda card: card['cards'] == start_cards,
+                                lookup_table.lookup_table[x])
             print(card_found)
             if len(card_found) > 0:
-               return str([(start_cards,card_found[0]['equity'])])
+                return str([(start_cards, card_found[0]['equity'])])
         print("{} not found in lookup_tables".format(start_cards))
-    print("{} {} {} will be calculated".format(start_cards,board,dead))
+    print("{} {} {} will be calculated".format(start_cards, board, dead))
     r = pbots_calc.calc(start_cards, board, dead, 1000000)
     if r:
         return str(zip(r.hands, r.ev))
-
