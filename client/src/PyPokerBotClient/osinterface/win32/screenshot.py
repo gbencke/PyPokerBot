f74c883e78330e3e36f65d777b3c18f17537238a
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-06-13 19:06:40 -0300

    Refactoring for the separation between screenshot grabbing and image handling

--- client/src/PyPokerBotClient/osinterface/win32/screenshot.py
+++ client/src/PyPokerBotClient/osinterface/win32/screenshot.py
@@ -112,36 +112,3 @@ def capture_screenshot(hwnd, file_to_save, should_save=True):
     return image_to_analyse
 
 
-def grab_image_from_file(image):
-    """
-    Loads a certain image from a filename and returns a numpy array representing it.
-
-    :param image: The filename to load.
-    :return: The numpy array representing the image
-    """
-    return Image.open(image)
-
-
-def grab_image_pos_from_image(image, pos, size):
-    """
-    Returns a region of the image based on a certain position and size.
-
-    :param image: The numpy array representing the image
-    :param pos: The Top-Left position
-    :param size: The size of the image
-    :return: A numpy array of the region, representing the image.
-    """
-    return image.crop((pos[0], pos[1], pos[0] + size[0], pos[1] + size[1]))
-
-
-def grab_image_pos_from_file(file_name, pos, size):
-    """
-    Returns a region of the image in the file name specified, based on a certain position and size
-
-    :param file_name: The file name of the image
-    :param pos: The Top-Left position
-    :param size: The size of the image
-    :return: A numpy array of the region, representing the image.
-    """
-    image_to_analyse = Image.open(file_name)
-    return grab_image_pos_from_image(image_to_analyse, pos, size)
