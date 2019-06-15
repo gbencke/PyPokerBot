5cae28f79b6e3c325c9e09ad7144d1475718a472
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-06-15 18:35:04 -0300

    Added Observer Params for the server

--- client/teste.simulator.no.debug.sh
+++ client/teste.simulator.no.debug.sh
@@ -2,5 +2,5 @@
 
 source ../env.simulator/bin/activate
 cd src
-python3 ./PyPokerBot.py simulator ../test/tables POKERSTARS 6-SEATS
+python3 ./PyPokerBot.py simulator ../test/tables POKERSTARS 6-SEATS --observer_url http://poker_app.ddns.net:5000/observe --tableid 1
 
