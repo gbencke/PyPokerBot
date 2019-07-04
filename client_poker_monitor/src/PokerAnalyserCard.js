93790f4e2de89371b17bba1a7fae4eb4d052ca9e
Guilherme <gbencke@benckesoftware.com.br>
2019-07-04 18:11:26 -0300

    Rendering the table cards statuses

--- client_poker_monitor/src/PokerAnalyserCard.js
+++ client_poker_monitor/src/PokerAnalyserCard.js
@@ -1,14 +1,72 @@
 import React, {Component} from 'react';
-import { Image, View, Text } from 'react-native';
+import { Dimensions, Image, View, Text } from 'react-native';
 import { Card } from 'react-native-material-ui';
 import { getCardCode } from './helpers/getCardCode';
+import FitImage from 'react-native-fit-image';
 
+const CardTemplate = require('./templates/card2.jpg');
 
 export default class PokerAnalyserCard extends Component {
+
   constructor(props){
     super(props)
     this.table = props.table;
-    this.tableType = `${this.table.image_platform} (${this.table.image_tabletype})`;
+  }
+
+  getTableType(){
+    return  `${this.table.image_platform} (${this.table.image_tabletype})`;
+  }
+
+  RenderPlayersCardsView(){
+    return (
+      <View style={styles.PlayersCardsView}>
+          { this.RenderPlayersCards() }
+      </View>
+    );
+  }
+
+  RenderCard(key){
+
+    const factor = 15; 
+
+    const PlayingCardViewStyle = { 
+      width : '16%', 
+      flexDirection: 'row' ,
+      justifyContent: 'space-between'
+    };
+
+    const RenderedCardStyle = { 
+      width: Dimensions.get('window').width / factor ,
+      height: Dimensions.get('window').height / factor 
+    };
+
+    return (
+      <View key={ key } style={ {flexDirection: 'column', width: '16%' }}>
+        <View style= { PlayingCardViewStyle }>
+          <FitImage
+            source = { CardTemplate }
+            style = { RenderedCardStyle } />
+          <FitImage
+            source = { CardTemplate }
+            style = { RenderedCardStyle } />
+        </View>
+        <Text style={{width:'100%', textAlign:'center'}}>AA</Text>
+      </View>
+    );
+  }
+
+  RenderNoCard(key){
+    return (
+      <View key={ key } style={{ flex: 1, justifyContent:'flex-end',alignItems:'center'}}>
+        <Text>No</Text>
+      </View>
+    )
+  }
+
+  RenderPlayersCards(){
+    return this.table.cards.map( (item, i) => {
+      return item ? this.RenderCard(i) : this.RenderNoCard(i) 
+    });
   }
 
   render(){
@@ -16,8 +74,8 @@ export default class PokerAnalyserCard extends Component {
       <View style={styles.ViewStyle}>
         <Card >
           <View style={styles.CardStyle}>
-            <Text style={styles.CardText}>{ this.tableType }</Text>
-            <Text style={styles.CardTextFont}>{ getCardCode('AsAh') }</Text>
+            <Text style={styles.CardText}>{ this.getTableType() }</Text>
+              { this.RenderPlayersCardsView() }
           </View>
         </Card>
       </View>
@@ -26,11 +84,16 @@ export default class PokerAnalyserCard extends Component {
 }
 
 const styles = {
+  PlayingCard: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
   ViewStyle: {
     width: '100%',
     paddingRight: 12,
     paddingLeft: 12,
     marginTop: 30,
+    height: 400
   },
   CardStyle : {
     marginLeft: 10,
@@ -42,6 +105,10 @@ const styles = {
     fontSize: 48,
     width: '100%',
     textAlign: 'center'
+  },
+  PlayersCardsView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   }
 };
 
