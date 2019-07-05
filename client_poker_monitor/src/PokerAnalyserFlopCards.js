63cb8f93306d8d1e3332681a98d511a122d52fdc
Guilherme <gbencke@benckesoftware.com.br>
2019-07-05 17:03:38 -0300

    Refactoring in more components

--- client_poker_monitor/src/PokerAnalyserFlopCards.js
+++ client_poker_monitor/src/PokerAnalyserFlopCards.js
@@ -1,3 +1,4 @@
+
 import React, {Component} from 'react';
 import { Dimensions, Image, View, Text } from 'react-native';
 import { Card } from 'react-native-material-ui';
@@ -6,12 +7,11 @@ import FitImage from 'react-native-fit-image';
 import PokerAnalyserPlayersCards from './PokerAnalyserPlayersCards';
 import PokerAnalyserHandStatus from './PokerAnalyserHandStatus';
 import PokerAnalyserHeroHand from './PokerAnalyserHeroHand';
-import PokerAnalyserFlopCards from './PokerAnalyserFlopCards';
 
 const CardTemplate = require('./templates/card2.jpg');
 const factor = 15; 
 
-export default class PokerAnalyserCard extends Component {
+export default class PokerAnalyserFlopCards extends Component {
 
   constructor(props){
     super(props)
@@ -20,11 +20,31 @@ export default class PokerAnalyserCard extends Component {
 
   render(){
     return (
-      <View style={{ flexDirection: "row" }}>
-        <PokerAnalyserHeroHand table={this.table}/>
-        <PokerAnalyserFlopCards table={this.table}/>
+      <View style={{ flexDirection: "column"}}>
+        <View style={styles.CardStyle}>
+          <Text>FLOP({`${this.table.hand_analisys.hand_phase}`})</Text>
+        </View>
+        <View style={styles.CardStyle}>
+          <Text style={styles.CardTextFont}>
+              { getCardCode(this.table.flop.join('')) }
+          </Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  CardStyle : {
+    marginLeft: 10,
+    marginRight:10
+  },
+  CardTextFont: {
+    marginTop: 10,
+    fontFamily: 'cards',
+    fontSize: 48,
+    width: '100%',
+    textAlign: 'center'
+  }
+};
 
--- client_poker_monitor/src/PokerAnalyserHeroHand.js
+++ client_poker_monitor/src/PokerAnalyserHeroHand.js
@@ -1,3 +1,4 @@
+
 import React, {Component} from 'react';
 import { Dimensions, Image, View, Text } from 'react-native';
 import { Card } from 'react-native-material-ui';
@@ -5,13 +6,11 @@ import { getCardCode } from './helpers/getCardCode';
 import FitImage from 'react-native-fit-image';
 import PokerAnalyserPlayersCards from './PokerAnalyserPlayersCards';
 import PokerAnalyserHandStatus from './PokerAnalyserHandStatus';
-import PokerAnalyserHeroHand from './PokerAnalyserHeroHand';
-import PokerAnalyserFlopCards from './PokerAnalyserFlopCards';
 
 const CardTemplate = require('./templates/card2.jpg');
 const factor = 15; 
 
-export default class PokerAnalyserCard extends Component {
+export default class PokerAnalyserHeroHand extends Component {
 
   constructor(props){
     super(props)
@@ -20,11 +19,31 @@ export default class PokerAnalyserCard extends Component {
 
   render(){
     return (
-      <View style={{ flexDirection: "row" }}>
-        <PokerAnalyserHeroHand table={this.table}/>
-        <PokerAnalyserFlopCards table={this.table}/>
+      <View style={{ flexDirection: "column"}}>
+        <View style={styles.CardStyle}>
+          <Text>CURRENT HAND:</Text>
+        </View>
+        <View style={styles.CardStyle}>
+          <Text style={styles.CardTextFont}>
+              { getCardCode(this.table.hero.hero_cards) }
+          </Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  CardStyle : {
+    marginLeft: 10,
+    marginRight:10
+  },
+  CardTextFont: {
+    marginTop: 10,
+    fontFamily: 'cards',
+    fontSize: 48,
+    width: '100%',
+    textAlign: 'center'
+  }
+};
 
