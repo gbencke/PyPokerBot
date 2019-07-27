717b6d9ba20d3e582110a661b9cb5865dc1c60e7
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-07-27 18:29:54 -0300

    Making the cards responsive

--- client_poker_monitor/src/ui/PokerAnalyserCardElements/PokerAnalyserHandStatus.js
+++ client_poker_monitor/src/ui/PokerAnalyserCardElements/PokerAnalyserHandStatus.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import { View } from "react-native";
 import PokerAnalyserHand from "./PokerAnalyserHand";
 import styled from "styled-components/native";
 
 const PokerAnalyserCardView = styled.View`
   margin-top: 20;
   flex-direction: row;
+  margin-left: 5%;
+  margin-right: 5%;
 `;
 
 export default PokerAnalyserCard = props => {
   return (
     <PokerAnalyserCardView>
-      <PokerAnalyserHand hero table={props.table} />
-      <PokerAnalyserHand flop table={props.table} />
+      <PokerAnalyserHand hero table={props.table} width={props.width}/>
+      <PokerAnalyserHand flop table={props.table} width={props.width}/>
     </PokerAnalyserCardView>
   );
 };
