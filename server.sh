0944b95e9572361f564a86978379d755e6e2568b
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-07-28 00:56:18 +0000

    Improvements on server shell

--- server.sh
+++ server.sh
@@ -5,9 +5,11 @@ then echo "Please run as root"
         exit
 fi
 
+export LD_LIBRARY_PATH=/usr/local/lib
+
 cd server/PyPokerBotAPI
 export FLASK_APP=server.py
 export FLASK_DEBUG=1
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 --port=80
 
 
