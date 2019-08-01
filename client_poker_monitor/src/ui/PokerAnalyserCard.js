7a1cf7c40a791d20367bb617ae56874bfce2a7f5
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-08-01 18:33:11 -0300

    Card style handling

--- client_poker_monitor/src/ui/PokerAnalyserCard.js
+++ client_poker_monitor/src/ui/PokerAnalyserCard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
+import { Card } from "react-native-material-ui";
 import PokerAnalyserPlayersCards from "./PokerAnalyserCardElements/PokerAnalyserPlayersCards";
 import PokerAnalyserHandStatus from "./PokerAnalyserCardElements/PokerAnalyserHandStatus";
 import PokerAnalyserCommands from "./PokerAnalyserCardElements/PokerAnalyserCommands";
@@ -9,23 +10,39 @@ import styled from "styled-components/native";
 
 const PokerAnalyserCardView = styled.View`
   width: ${props => props.width}px;
-  ${Platform.OS === "ios" ? "margin-top: 30px;" : "margin:0 0 0 0;"}: 
+  ${Platform.OS === "ios" ? "margin-top: 30px;" : "margin:0 0 0 0;"} 
   ${props => (props.noMargin !== true ? "padding-right: 12;" : null)}
   ${props => (props.noMargin !== true ? "padding-left: 12;" : null)}
-  ${props => (props.noMargin !== true ? "margin-top: 30;" : null)}
+  ${props => (props.noMargin !== true ? "margin-top: 0;" : null)}
   height: 400;
 `;
 
 export default PokerAnalyserCard = props => {
   if (!props.table || props.width < 100 || props.width > 1000) return null;
 
+  getCardComponents = () => {
+    return (
+      <View>
+        <PokerAnalyserTableType width={props.width} table={props.table} />
+        <PokerAnalyserPlayersCards width={props.width} table={props.table} />
+        <PokerAnalyserHandStatus width={props.width} table={props.table} />
+        <PokerAnalyserCommands width={props.width} table={props.table} />
+        <PokerAnalyserDecision width={props.width} table={props.table} />
+      </View>
+    );
+  };
+
+  resolveCard = () => {
+    if (props.noMargin) {
+      return getCardComponents();
+    } else {
+      return <Card>{getCardComponents()}</Card>;
+    }
+  };
+
   return (
     <PokerAnalyserCardView width={props.width} noMargin={props.noMargin}>
-      <PokerAnalyserTableType width={props.width} table={props.table} />
-      <PokerAnalyserPlayersCards width={props.width} table={props.table} />
-      <PokerAnalyserHandStatus width={props.width} table={props.table} />
-      <PokerAnalyserCommands width={props.width} table={props.table} />
-      <PokerAnalyserDecision width={props.width} table={props.table} />
+      {resolveCard()}
     </PokerAnalyserCardView>
   );
 };
